5e9e4c70032dac4b88e62611150cfef2a8492d1d
Shazad1 <shahzadhere@gmail.com>
2023-07-08 15:42:19 +1000

    initial draft

--- App.js
+++ App.js
@@ -1,11 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Camera, CameraType } from 'expo-camera';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const [type, setType] = useState(CameraType.back);
+  const [zoom, setZoom] = useState(0);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+
+
+  if (!permission) {
+    // Camera permissions are still loading
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
+        <Button style={{backgroundColor: '#ffde59', color:'#000'}} onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function increaseZoom() {
+    if (zoom <= 0.9) {
+      setZoom(zoom + 0.1)
+    }
+  }
+  function decreaseZoom() {
+    if (zoom >= 0.1) {
+      setZoom(zoom - 0.1)
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera style={styles.camera} type={type} autoFocus = {true} zoom={zoom}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={increaseZoom}>
+            <Text style={styles.text}>+</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.button} onPress={decreaseZoom}>
+            <Text style={styles.text}>-</Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
     </View>
   );
 }
@@ -13,8 +54,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
   },
 });
